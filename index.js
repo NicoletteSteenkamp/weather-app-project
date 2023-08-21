244ad1cb443f5982c4ff84dc441daa67b13ad641
NicoletteSteenkamp <steenkampnicolette815@gmail.com>
2023-08-21 02:45:21 +0200

    JS

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function displayTemperature(response) {
-  console.log(response);
+  console.log(response.data.main);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature;
 }
 let apiKey = "ae997t30869fc345038bf7f0abaao7e6";
 let apiUrl =
   "https://api.shecodes.io/weather/v1/current?query=Johannesburg&key=ae997t30869fc345038bf7f0abaao7e6&units=metric";
-axios.get(url).then(displayTemperature);
+axios.get(apiUrl).then(displayTemperature);
