037e7647b7918f71eea81f97272adda67709e0bc
NicoletteSteenkamp <steenkampnicolette815@gmail.com>
2023-08-25 14:26:07 +0200

    updates css, added some JS

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ let apiKey = "ae997t30869fc345038bf7f0abaao7e6";
 let apiUrl =
   "https://api.shecodes.io/weather/v1/current?query=Johannesburg&key=ae997t30869fc345038bf7f0abaao7e6&units=metric";
 axios.get(apiUrl).then(displayTemperature);
+
+function timeUpdate() {
+  let now = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+}
