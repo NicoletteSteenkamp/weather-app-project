0232908da4bba7bd09922a9baa53c14136479688
NicoletteSteenkamp <steenkampnicolette815@gmail.com>
2023-08-27 01:55:08 +0200

    updated date func

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.temperature.day);
+  temperatureElement.innerHTML = Math.round(response.data.temperature);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
   let descriptionElement = document.querySelector("#description");
@@ -10,20 +10,27 @@ function displayTemperature(response) {
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  let timeElement = document.querySelector("#time");
-  timeElement.innerHTML = formatDate(response.data.time * 1000);
-
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute("src", response.data.condition.icon_url);
   iconElement.setAttribute("alt", response.data.condition.description);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "ae997t30869fc345038bf7f0abaao7e6";
 
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Boksburg&key=ae997t30869fc345038bf7f0abaao7e6&units=metric;`;
 axios.get(apiUrl).then(displayTemperature);
 
-function timeUpdate() {
-  let now = new Date();
+function formatDate(timeStamp) {
+  let date = new date(timeStamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0 ${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0 ${minutes}`;
+  }
   let days = [
     "Sunday",
     "Monday",
@@ -33,14 +40,6 @@ function timeUpdate() {
     "Friday",
     "Saturday",
   ];
-  let day = days[now.getDay()];
-  let hour = now.getHours();
-  let minutes = now.getMinutes();
-
-  if (hour < 10) {
-    hour = "0" + hour;
-  }
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
