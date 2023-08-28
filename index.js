a44254a673bc196f34271f34b9a6d123a9e098a5
NicoletteSteenkamp <steenkampnicolette815@gmail.com>
2023-08-28 14:56:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,5 +57,5 @@ function displayTemperature(response) {
 }
 let apiKey = "ae997t30869fc345038bf7f0abaao7e6";
 
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Boksburg&key=ae997t30869fc345038bf7f0abaao7e6&units=metric;`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Boksburg&key=ae997t30869fc345038bf7f0abaao7e6&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
